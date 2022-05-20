af532b4dbed5275f02365d6e6035e52b49cb5cfa
Kupriienko <Kupriienko>
2022-05-20 21:07:49 +0300

    updated

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request, Response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    with open('contents.html') as contents:
+        return contents.read()
+
+
+@app.route('/add', methods=['POST'])
+def add() -> Response:
+    data = request.get_json()
+    return jsonify(
+        total=sum(data.values()),
+    )
+
+
+@app.route('/mult', methods=['POST'])
+def mult() -> Response:
+    data = request.get_json()
+    return jsonify(
+        total=data['a'] * data['b'] * data['c'],
+    )
+
+
+@app.route('/min_and_max', methods=['POST'])
+def min_and_max() -> Response:
+    data = request.get_json()
+
+    return jsonify(
+        total=[min(data.values()), max(data.values())]
+    )
